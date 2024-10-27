df50afb3a401fdb8d21a48938b9ba76be44f8dac
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-10-27 14:35:12 +0100

    Dependency files renamed

--- app/main.cpp
+++ app/main.cpp
@@ -12,19 +12,6 @@ po::options_description CreateOptionsDescription()
 		("service-install", "install .service file. Use --config to specify path to config file.")
 		("service-uninstall", "uninstall .service file")
 		("config", po::value<std::string>(), "path to config file")
-		("ip", po::value<std::string>()->default_value("10.10.0.1"), "listening IP")
-		("port", po::value<uint16_t>()->default_value(10001), "listening port of Sensor Collector")
-		("port-time", po::value<uint16_t>(), "listening port of Time Provider")
-		("console-log-only", "allows to disable logging to file")
-		("log-dir", po::value<std::string>(), "logging directory")
-		("log-level", po::value<std::string>()->default_value("INFO"), "logging level")
-		("force-mkdirs", "create missing directories")
-		("registry-provider", po::value<std::string>(), "Sensor Registry driver name. Supported providers: csv, mysql")
-		("registry-string", po::value<std::string>(), "Sensor Registry Connection String")
-		("database-provider", po::value<std::string>(), "Sensor Database driver name. Supported providers: json, mysql, influxdb")
-		("database-string", po::value<std::string>(), "Sensor Database Connection String passed to selected provider")
-		("psk", po::value<std::string>(), "Pre-shared encryption key")
-		("psk-path", po::value<std::string>(), "Path to a text file containing pre-shared encryption key")
 		;
 
 	return desc;
@@ -32,7 +19,7 @@ po::options_description CreateOptionsDescription()
 
 int main()
 {	
-	spdlog::info("Hello World!");
+	spdlog::info("Display Manager started");
 	po::options_description optionsDesc = CreateOptionsDescription();
 
 	return 0;
